340f42ae45527da641f35e18c28635de0a7a9bdb
berorev <berorev@gmail.com>
2019-05-18 00:46:26 +0900

    modify response

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/*', (req, res) => {
-  console.log(req.path);
-  console.log(req.query);
-  res.send('Hello World!');
+  res.send(`path: ${req.path}<br>\nquery: ${JSON.stringify(req.query, null, 2)}`);
 });
 
 const port = process.env.PORT || 3000;
