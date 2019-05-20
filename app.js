2fbd052b3ca57061aa5b519587f114b681cd675d
berorev <berorev@gmail.com>
2019-05-20 20:10:14 +0900

    add signature checks

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
+// https://github.com/slackapi/template-slash-command-and-dialogs/blob/master/src/index.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const slackUtils = require('./slack_utils');
+
+if (!process.env.SLACK_SIGNING_SECRET) {
+  console.error('env.SLACK_SIGNING_SECRET not defined');
+  process.exit(1);
+}
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extends: true }));
+/*
+ * Parse application/x-www-form-urlencoded && application/json
+ * Use body-parser's `verify` callback to export a parsed raw body
+ * that you need to use to verify the signature
+ */
+const rawBodyBuffer = (req, res, buf, encoding) => {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || 'utf8');
+  }
+};
+
+app.use(bodyParser.urlencoded({ verify: rawBodyBuffer, extended: true }));
+app.use(bodyParser.json({ verify: rawBodyBuffer }));
 
 app.post('/*', (req, res) => {
-  const { path } = req;
-  const queryJson = JSON.stringify(req.query);
-  const bodyJson = JSON.stringify(req.body);
-  res.send(`path: ${path}\nquery: ${queryJson}\nbody: ${bodyJson}`);
-
-  // error
-  // res.contentType('application/json');
-  // {
-  //   "response_type": "ephemeral",
-  //   "text": "Sorry, that didn't work. Please try again."
-  // }
+  if (slackUtils.isVerified(req)) {
+    const { path } = req;
+    const queryJson = JSON.stringify(req.query);
+    const bodyJson = JSON.stringify(req.body);
+    res.send(`path: ${path}\nquery: ${queryJson}\nbody: ${bodyJson}`);
+  } else {
+    // error
+    res.status(401).json({
+      response_type: 'ephemeral',
+      text: 'Verification token mismatch'
+    });
+  }
 });
 
 const port = process.env.PORT || 3000;
