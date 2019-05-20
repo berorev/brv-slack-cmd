1ea38426e0869b4253ab5f42f472ec0dba3416ce
berorev <berorev@gmail.com>
2019-05-20 18:26:44 +0900

    use bodyParser

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extends: true }));
+
 app.post('/*', (req, res) => {
-  console.log(`path: ${req.path}<br>\nquery: ${JSON.stringify(req.query)}`);
-  res.send(`path: ${req.path}<br>\nquery: ${JSON.stringify(req.query, null, 2)}`);
+  res.send(`path: ${req.path}\n`);
+  res.send(`query: ${JSON.stringify(req.query, null, 2)}\n`);
+  res.send(`body: ${JSON.stringify(req.body, null, 2)}`);
+
+  // error
+  // res.contentType('application/json');
+  // {
+  //   "response_type": "ephemeral",
+  //   "text": "Sorry, that didn't work. Please try again."
+  // }
 });
 
 const port = process.env.PORT || 3000;
